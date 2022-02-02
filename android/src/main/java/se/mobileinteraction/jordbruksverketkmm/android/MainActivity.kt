1c092d88a429ddf9d18354610ed7a2245d39a223
ghanaMI <ghanavino@Ghanavinodhinis-MacBook-Air.local>
2022-02-02 11:28:39 +0100

    APJ-21_added custom toolbar

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/MainActivity.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import se.mobileinteraction.jordbruksverketkmm.Greeting
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import se.mobileinteraction.jordbruksverketkmm.android.fragments.AboutAppFragment
+
 
 fun greet(): String {
     return Greeting().greeting()
@@ -14,7 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
+        val customMenuItemToolbar : Toolbar = findViewById(R.id.custom_menuItem_toolbar)
+        val toolbarTitle : TextView = findViewById(R.id.toolbar_titleText)
+        val toolbarNavText : TextView = findViewById(R.id.toolbar_navigationText)
+
+        customMenuItemToolbar.setTitle("")
+        toolbarTitle.setText("OM APPEN")
+        toolbarNavText.setText("TILLBAKA")
+        setSupportActionBar(customMenuItemToolbar)
+
+        //val tv: TextView = findViewById(R.id.text_view)
         //tv.text = greet()
 
         displayAboutApp()
