d8d265f7c0c55c8366c033ef0b9a6b784a475c5c
jonasbergstrom <jontrian97@gmail.com>
2022-02-04 13:19:55 +0100

    Made the Start Screen and tested the buttons for each one

--- shared/src/commonTest/kotlin/se/mobileinteraction/jordbruksverketkmm/commonTest.kt
+++ shared/src/commonTest/kotlin/se/mobileinteraction/jordbruksverketkmm/commonTest.kt
@@ -7,6 +7,6 @@ class CommonGreetingTest {
 
     @Test
     fun testExample() {
-        assertTrue(Greeting().greeting().contains("Hello"), "Check 'Hello' is mentioned")
+        assertTrue(MainLabel().greeting().contains("Hello"), "Check 'Hello' is mentioned")
     }
 }
