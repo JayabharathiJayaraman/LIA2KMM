e735a6e960f6981ea2e84ddfc7a9d2ca73f6f0a0
jonasbergstrom <jontrian97@gmail.com>
2022-02-15 17:16:24 +0100

    Created the start screen for iOS

--- ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
@@ -1,12 +1,32 @@
 import UIKit
 
 class StartScreenViewController: UIViewController {
-
+    
+    @IBOutlet weak var welcomelabel: UILabel!
+    
+    @IBOutlet weak var appnamelabel: UILabel!
+        
+    @IBOutlet weak var myteslabel: UILabel!
+    
+    @IBOutlet weak var newtestlabel: UILabel!
+    
+    @IBOutlet weak var planticonlabel1: UILabel!
+    
+    @IBOutlet weak var planticonlabel2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        welcomelabel.text = "WelcomeLabel".localized
+        appnamelabel.text = "AppNameLabel".localized
+        myteslabel.text = "MyTestLabel".localized
+        newtestlabel.text = "NewTestLabel".localized
+        planticonlabel1.text = "PlantLabel1".localized
+        planticonlabel2.text = "PlantLabel2".localized
+    
 
-        // Do any additional setup after loading the view.
+    }
+    
     }
 
-
-}
