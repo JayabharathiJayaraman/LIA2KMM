cd60bc74adf4a33488b8dd5c974b44a61d38618a
jonasbergstrom <jontrian97@gmail.com>
2022-02-17 14:50:02 +0100

    Added dynamic fonts for the text labels and added some constrains also

--- ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
@@ -1,32 +1,71 @@
 import UIKit
 
+
+extension UILabel {
+  func dynamicFont(){
+    guard let customFont = UIFont(name: "OpenSans-Bold", size: UIFont.labelFontSize) else {
+      fatalError("""
+        Failed to load the "OpenSans-Bold" font.
+        Make sure the font file is included in the project and the font name is spelled correctly.
+        """
+      )
+    }
+    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
+    self.adjustsFontForContentSizeCategory = true
+  }
+}
+
+
+extension UILabel {
+  func dynamicFont2(){
+    guard let customFont = UIFont(name: "OpenSans-SemiBold", size: UIFont.labelFontSize) else {
+      fatalError("""
+        Failed to load the "OpenSans-SemiBold" font.
+        Make sure the font file is included in the project and the font name is spelled correctly.
+        """
+      )
+    }
+    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
+    self.adjustsFontForContentSizeCategory = true
+
+  }
+}
+
 class StartScreenViewController: UIViewController {
     
     @IBOutlet weak var welcomelabel: UILabel!
     
     @IBOutlet weak var appnamelabel: UILabel!
-        
-    @IBOutlet weak var myteslabel: UILabel!
+            
+    @IBOutlet weak var mytestlabel: UILabel!
     
     @IBOutlet weak var newtestlabel: UILabel!
     
-    @IBOutlet weak var planticonlabel1: UILabel!
     
-    @IBOutlet weak var planticonlabel2: UILabel!
+    @IBOutlet weak var planticonlabel: UILabel!
     
     
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        welcomelabel.text = "WelcomeLabel".localized
-        appnamelabel.text = "AppNameLabel".localized
-        myteslabel.text = "MyTestLabel".localized
-        newtestlabel.text = "NewTestLabel".localized
-        planticonlabel1.text = "PlantLabel1".localized
-        planticonlabel2.text = "PlantLabel2".localized
+        welcomelabel.dynamicFont2()
+        mytestlabel.dynamicFont()
+        newtestlabel.dynamicFont()
+        planticonlabel.dynamicFont()
+
     
+        func localized(){
+        
+            welcomelabel.text = "WelcomeLabel".localized
+            appnamelabel.text = "AppNameLabel".localized
+            mytestlabel.text = "MyTestLabel".localized
+            newtestlabel.text = "NewTestLabel".localized
+            planticonlabel.text = "PlantLabel1".localized
+
+        }
 
     }
     
-    }
+}
 
