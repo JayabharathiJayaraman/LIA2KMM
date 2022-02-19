c216f0bb9085f04f56fc02b455d7f05316d11c97
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-02-20 00:15:45 +0100

    Function takephoto() now saves new image file to internal memory using File Api

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/ui/camera/CameraFragment.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/ui/camera/CameraFragment.kt
@@ -1,23 +1,19 @@
 package se.mobileinteraction.jordbruksverketkmm.android.ui.camera
 
-import android.content.ContentValues
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import se.mobileinteraction.jordbruksverketkmm.android.databinding.CameraUiContainerBinding
 import se.mobileinteraction.jordbruksverketkmm.android.databinding.FragmentCameraBinding
+import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
@@ -43,16 +39,12 @@ class CameraFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         cameraExecutor = Executors.newSingleThreadExecutor()
-
         fragmentCameraBinding.viewFinder.post{
-
             startCamera()
             _fragmentCameraBinding?.imageCaptureButton?.setOnClickListener {
                 takePhoto()
             }
-
         }
-
     }
 
     private fun startCamera() {
@@ -88,61 +80,49 @@ class CameraFragment : Fragment() {
         }, ContextCompat.getMainExecutor(activity?.applicationContext!!))
     }
 
-
     private fun takePhoto() {
         // Get a stable reference of the modifiable image capture use case
         imageCapture ?: return
 
-        // Create time stamped name and MediaStore entry.
+        // Create time stamped name and File entry.
         val name = SimpleDateFormat(FILENAME_FORMAT,  Locale.US)
             .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            /*if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Jordbruksverket-Image")
-            }*/
-        }
+        val photoFile = File(context?.filesDir, "$name.jpg")
 
         // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(activity?.contentResolver!!,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
-            .build()
+        val outputOptions = activity?.contentResolver?.let {
+            ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        }
 
         // Set up image capture listener, which is triggered after photo has
         // been taken
-        imageCapture!!.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(activity?.applicationContext!!),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e("DEBUG", "Photo capture failed: ${exc.message}", exc)
-                }
+        outputOptions?.let {
+            imageCapture?.takePicture(
+                it,
+                ContextCompat.getMainExecutor(requireContext()),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e("DEBUG", "Photo capture failed: ${exc.message}", exc)
+                    }
 
-                override fun
-                        onImageSaved(output: ImageCapture.OutputFileResults){
-                    val msg = "Photo capture succeeded: ${output.savedUri}"
-                   // Toast.makeText(activity?.applicationContext, msg, Toast.LENGTH_SHORT).show()
-                    updateUiForImagePreview(output.savedUri)
-
-                    Log.d("DEBUG", msg)
+                    override fun
+                            onImageSaved(output: ImageCapture.OutputFileResults){
+                        val msg = "Photo capture succeeded: ${output.savedUri}"
+                        // Toast.makeText(activity?.applicationContext, msg, Toast.LENGTH_SHORT).show()
+                        updateUiForImagePreview(output.savedUri)
+                        Log.d("DEBUG", msg)
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 
-
-
     private fun updateUiForImagePreview(outputUri: Uri?) {
-
         cameraUiContainerBinding = CameraUiContainerBinding.inflate(
             LayoutInflater.from(requireContext()),
             fragmentCameraBinding.root,
             true
         )
-
         _fragmentCameraBinding?.imageCaptureButton?.isVisible = false
         _fragmentCameraBinding?.viewFinder?.isVisible = false
         cameraUiContainerBinding?.ivPreview?.setImageURI(outputUri)
@@ -153,7 +133,6 @@ class CameraFragment : Fragment() {
         cameraUiContainerBinding?.btnDeclineImage?.setOnClickListener {
             updateUiForDeclineImage()
         }
-
     }
 
     private fun updateUiForDeclineImage() {
@@ -162,8 +141,6 @@ class CameraFragment : Fragment() {
             fragmentCameraBinding.root.removeView(it)
             _fragmentCameraBinding?.imageCaptureButton?.isVisible = true
             _fragmentCameraBinding?.viewFinder?.isVisible = true
-
-
         }
     }
 
@@ -171,7 +148,6 @@ class CameraFragment : Fragment() {
         TODO("Implement sending Uri to fragment for viewing the image")
 
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _fragmentCameraBinding = null
