fa3fe9d773bef2436b95e6a137695efb7ce32c82
jonasbergstrom <jontrian97@gmail.com>
2022-02-20 17:14:17 +0100

    Started with the Map Screen, Added a navigation bar on top, the MKMapView and the labels/buttons for the screen

---
+++ ios/JordbruksverketKMMiOS/ViewControllers/MapScreenViewController/MapScreenViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+import MapKit
+
+class MapScreenViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
