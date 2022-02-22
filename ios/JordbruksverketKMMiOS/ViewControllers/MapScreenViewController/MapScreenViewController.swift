aa200503616672ac0fcf4ed0ff85df205ba7894b
jonasbergstrom <jontrian97@gmail.com>
2022-02-22 15:09:50 +0100

    Changed it to satellite mode and also fixed so it zooms in on the current location when the user approves it

--- ios/JordbruksverketKMMiOS/ViewControllers/MapScreenViewController/MapScreenViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/MapScreenViewController/MapScreenViewController.swift
@@ -3,66 +3,75 @@ import MapKit
 
 class MapScreenViewController: UIViewController, CLLocationManagerDelegate {
     
-    
     @IBOutlet weak var mapView: MKMapView!
     
-
     let locationManger = CLLocationManager()
-
-       
-
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           checkLocationServices()
-           
-         
-       }
+    let regionInMeters: Double = 200
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocationServices()
+        mapView.mapType = .satellite
+    }
     
     override var shouldAutorotate: Bool{
-         return false
-     }
-     
-     override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
-         return .portrait
-     }
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return .portrait
+    }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-
+        
         guard !annotation.isKind(of: MKUserLocation.self) else { return nil }
-
-
+        
+        
         return MKAnnotationView()
     }
-       
-       func setupLocationManger() {
-           locationManger.delegate = self
-           locationManger.desiredAccuracy = kCLLocationAccuracyBest
-       }
-       
-       func checkLocationServices() {
-           if CLLocationManager.locationServicesEnabled(){
-               setupLocationManger()
-               checkLocationAuthorization()
-           } else {
-               
-           }
-       }
-       
-       func checkLocationAuthorization() {
-           switch CLLocationManager.authorizationStatus() {
-           case .authorizedWhenInUse:
-               mapView.showsUserLocation = true
-               break
-           case .denied:
-               break
-           case .notDetermined:
-               locationManger.requestWhenInUseAuthorization()
-           case .restricted:
-               break
-           case .authorizedAlways:
-               break
-           }
-       }
-
-
-   }
+    
+    func setupLocationManger() {
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func zoomInViewOnUserLocation () {
+        if let location = locationManger.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+    }
+}
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManger()
+            checkLocationAuthorization()
+            zoomInViewOnUserLocation()
+            
+        } else {
+            
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            locationManger.delegate = self
+            locationManger.startUpdatingLocation()
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManger.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationServices()
+    }
+}
