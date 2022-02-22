48d36642d8262988d4b8b2bc3052c35837ef6da6
jonasbergstrom <jontrian97@gmail.com>
2022-02-22 11:27:04 +0100

    Added the code for show the user's location, also locked the screen in portrait mode

--- ios/JordbruksverketKMMiOS/ViewControllers/MapScreenViewController/MapScreenViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/MapScreenViewController/MapScreenViewController.swift
@@ -1,17 +1,68 @@
 import UIKit
 import MapKit
 
-class MapScreenViewController: UIViewController {
+class MapScreenViewController: UIViewController, CLLocationManagerDelegate {
     
     
     @IBOutlet weak var mapView: MKMapView!
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let locationManger = CLLocationManager()
+
+       
+
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           checkLocationServices()
+           
+         
+       }
+    
+    override var shouldAutorotate: Bool{
+         return false
+     }
+     
+     override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+         return .portrait
+     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        guard !annotation.isKind(of: MKUserLocation.self) else { return nil }
+
+
+        return MKAnnotationView()
     }
+       
+       func setupLocationManger() {
+           locationManger.delegate = self
+           locationManger.desiredAccuracy = kCLLocationAccuracyBest
+       }
+       
+       func checkLocationServices() {
+           if CLLocationManager.locationServicesEnabled(){
+               setupLocationManger()
+               checkLocationAuthorization()
+           } else {
+               
+           }
+       }
+       
+       func checkLocationAuthorization() {
+           switch CLLocationManager.authorizationStatus() {
+           case .authorizedWhenInUse:
+               mapView.showsUserLocation = true
+               break
+           case .denied:
+               break
+           case .notDetermined:
+               locationManger.requestWhenInUseAuthorization()
+           case .restricted:
+               break
+           case .authorizedAlways:
+               break
+           }
+       }
 
 
-
-}
+   }
