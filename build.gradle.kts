561f6dcafda1b30ca3b9f0b3eee96c9c345fa44f
Erik H <erik.hedrenius@mobileinteraction.se>
2022-02-25 10:15:48 +0100

    Add coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,9 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+        classpath("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+        classpath("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     }
 }
 
