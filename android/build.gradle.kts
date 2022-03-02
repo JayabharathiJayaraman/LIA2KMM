0bc60f1b075f9e2cefe4ef40e2fbc0f6512e1e7a
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-03-02 12:20:56 +0100

    Removed empty rows

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -26,7 +26,6 @@ android {
     }
 }
 
-
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
@@ -35,15 +34,12 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-
     //Camera X
     val cameraxVersion = "1.1.0-beta01"
-
     implementation("androidx.camera:camera-core:$cameraxVersion")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
     implementation("androidx.camera:camera-extensions:$cameraxVersion")
     implementation("androidx.activity:activity-ktx:1.5.0-alpha02")
-
 }
