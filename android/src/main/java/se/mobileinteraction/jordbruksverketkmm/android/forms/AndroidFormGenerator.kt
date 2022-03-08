b38e81694673642e69261485662091af0976033b
Erik H <erik.hedrenius@mobileinteraction.se>
2022-03-08 09:45:35 +0100

    Use viewGroup extensions

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/forms/AndroidFormGenerator.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/forms/AndroidFormGenerator.kt
@@ -3,38 +3,29 @@ package se.mobileinteraction.jordbruksverketkmm.android.forms
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import se.mobileinteraction.jordbruksverketkmm.android.R
 import se.mobileinteraction.jordbruksverketkmm.forms.components.*
 
-
 class AndroidFormGenerator(val context: Context) : FormGenerator {
-    var mainView: LinearLayout
-
-    init {
-        val linearLayout = LinearLayout(context)
-        this.mainView = linearLayout
-    }
-
     override fun generateInterface(components: List<FormComponent>): View {
-        val linearLayout = LinearLayout(context)
+        val mainView = LinearLayout(context)
         val params: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        linearLayout.layoutParams = params
-        linearLayout.orientation = LinearLayout.VERTICAL
-
-        mainView = linearLayout
+        mainView.layoutParams = params
+        mainView.orientation = LinearLayout.VERTICAL
 
         for (component in components) {
             when (component.type) {
                 ComponentType.BODY -> {
                     val body = (component as FormComponentText)
-                    addBodyLabel(body.text)
+                    mainView.addBodyLabel(body.text)
                 }
                 ComponentType.IMAGE -> {
                     val image = (component as FormComponentImage)
-                    addImage(image.image, image.caption)
+                    mainView.addImage(image.image, image.caption)
                 }
                 else -> println("unknown")
             }
@@ -42,48 +33,45 @@ class AndroidFormGenerator(val context: Context) : FormGenerator {
 
         return mainView
     }
+}
 
-    fun addBigTitleLabel(text: String) {
-    }
+private fun ViewGroup.addBigTitleLabel(text: String) {
+}
 
-    fun addSmallTitleLabel(text: String) {
-    }
+private fun ViewGroup.addSmallTitleLabel(text: String) {
+}
 
-    fun addBodyLabel(text: String) {
-        val textView = TextView(context)
-        textView.text = text
+private fun ViewGroup.addBodyLabel(text: String) {
+    val textView = TextView(context)
+    textView.text = text
 
-        mainView.addView(textView)
-    }
+    this.addView(textView)
+}
 
-    fun addTextField(id: String, text: String, placeholder: String) {
-    }
+private fun ViewGroup.addTextField(id: String, text: String, placeholder: String) {
+}
 
-    fun addButtonList(
-        id: String,
-        title: String,
-        list: List<String>,
-        value: String,
-        placeholder: String
-    ) {
-    }
+private fun ViewGroup.addButtonList(
+    id: String,
+    title: String,
+    list: List<String>,
+    value: String,
+    placeholder: String
+) {
+}
 
-    fun addImage(imageName: String, caption: String) {
-        val imageView = ImageView(context)
-        imageView.setImageResource(getImageResource(imageName))
+private fun ViewGroup.addImage(imageName: String, caption: String) {
+    val imageView = ImageView(context)
+    imageView.setImageResource(getImageResource(imageName))
 
-        mainView.addView(imageView)
+    this.addView(imageView)
 
-        val textView = TextView(context)
-        textView.text = caption
+    val textView = TextView(context)
+    textView.text = caption
 
-        mainView.addView(textView)
-    }
+    this.addView(textView)
+}
 
-    fun getImageResource(name: String): Int {
-        return context.resources.getIdentifier("drawable/$name", null, context.packageName)
-    }
-
-    fun clear() {
-    }
+private fun ViewGroup.getImageResource(name: String): Int {
+    return context.resources.getIdentifier("drawable/$name", null, context.packageName)
 }
