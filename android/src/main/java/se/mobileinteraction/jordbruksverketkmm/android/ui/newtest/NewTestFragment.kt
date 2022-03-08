885c12e6e97c6dbedea44d95003fd6992c2848df
David Dahlman <daviddahlman82@gmail.com>
2022-03-08 13:36:43 +0100

    Removed onCreate function in NewTestFragment.kt

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/ui/newtest/NewTestFragment.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/ui/newtest/NewTestFragment.kt
@@ -13,11 +13,6 @@ class NewTestFragment : Fragment() {
 
     private var fragmentNewTestBinding: FragmentNewTestBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
