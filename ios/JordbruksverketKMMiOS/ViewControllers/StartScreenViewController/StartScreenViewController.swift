a75996b513432c757d1b0d415978b1bbf766bad7
jonasbergstrom <jontrian97@gmail.com>
2022-03-08 11:46:13 +0100

    move extensions to Common Extensions

--- ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import shared
 
 extension UILabel {
     
@@ -15,61 +16,7 @@ extension UILabel {
   }
 }
 
-extension UILabel {
-  func dynamicFontSemiBoldWelcome(){
-    guard let customFont = UIFont(name: "opensans-semibold", size: 16) else {
-      fatalError("""
-        Failed to load the "opensans-semibold" font.
-        Make sure the font file is included in the project and the font name is spelled correctly.
-        """
-      )
-    }
-    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
-    self.adjustsFontForContentSizeCategory = true
-  }
-}
 
-extension UILabel {
-  func dynamicFontSemiBold(){
-      guard let customFont = UIFont(name: "opensans-semibold", size: 14) else {
-      fatalError("""
-        Failed to load the "opensans-semibold" font.
-        Make sure the font file is included in the project and the font name is spelled correctly.
-        """
-      )
-    }
-    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
-    self.adjustsFontForContentSizeCategory = true
-  }
-}
-
-extension UILabel {
-  func dynamicFontSemiBoldAbout(){
-      guard let customFont = UIFont(name: "opensans-semibold", size: 16.5) else {
-      fatalError("""
-        Failed to load the "opensans-semibold" font.
-        Make sure the font file is included in the project and the font name is spelled correctly.
-        """
-      )
-    }
-    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
-    self.adjustsFontForContentSizeCategory = true
-  }
-}
-
-extension UILabel {
-  func dynamicFontRegular(){
-    guard let customFont = UIFont(name: "opensans-regular", size: UIFont.labelFontSize) else {
-      fatalError("""
-        Failed to load the "opensans-regular" font.
-        Make sure the font file is included in the project and the font name is spelled correctly.
-        """
-      )
-    }
-    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
-    self.adjustsFontForContentSizeCategory = true
-  }
-}
 
 final class StartScreenViewController: UIViewController {
     
