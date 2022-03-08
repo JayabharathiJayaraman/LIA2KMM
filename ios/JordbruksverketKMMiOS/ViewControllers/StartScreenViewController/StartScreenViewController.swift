1e75c263797b0657b8bc49eeae55eb6aa9b89b98
jonasbergstrom <jontrian97@gmail.com>
2022-03-08 13:26:41 +0100

    Moved the extensions to the common folder in swift

--- ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/StartScreenViewController/StartScreenViewController.swift
@@ -1,23 +1,6 @@
 import UIKit
 import shared
 
-extension UILabel {
-    
-  func dynamicFontBold(){
-    guard let customFont = UIFont(name: "opensans-bold", size: UIFont.labelFontSize) else {
-      fatalError("""
-        Failed to load the "opensans-bold" font.
-        Make sure the font file is included in the project and the font name is spelled correctly.
-        """
-      )
-    }
-    self.font = UIFontMetrics(forTextStyle: .headline).scaledFont(for: customFont)
-    self.adjustsFontForContentSizeCategory = true
-  }
-}
-
-
-
 final class StartScreenViewController: UIViewController {
     
     @IBOutlet private weak var welcomelabel: UILabel!
