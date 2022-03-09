8fa68994e213187928d34bef0573560ad3c39e52
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-03-09 18:45:59 +0100

    Change form type for testing FormSoilStructureQuestions

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/MainApplication.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/MainApplication.kt
@@ -2,9 +2,9 @@ package se.mobileinteraction.jordbruksverketkmm.android
 
 import android.app.Application
 import se.mobileinteraction.jordbruksverketkmm.forms.FormViewModel
-import se.mobileinteraction.jordbruksverketkmm.forms.forms.FormGeneralQuestions
+import se.mobileinteraction.jordbruksverketkmm.forms.forms.FormSoilStructureQuestions
 import se.mobileinteraction.jordbruksverketkmm.forms.forms.FormType
 
 class MainApplication : Application() {
-    val formViewModel: FormViewModel = FormViewModel(FormGeneralQuestions(FormType.GeneralQuestions))
+    val formViewModel: FormViewModel = FormViewModel(FormSoilStructureQuestions(FormType.SoilStructure))
 }
