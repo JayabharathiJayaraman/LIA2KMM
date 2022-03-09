97a89bc3bf995eee81a25036912de063556e2f45
Erik H <erik.hedrenius@mobileinteraction.se>
2022-03-09 10:13:12 +0100

    Add checklist files

---
+++ shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/checklists/models/Checklist.kt
@@ -0,0 +1,17 @@
+package se.mobileinteraction.jordbruksverketkmm.checklists.models
+
+import se.mobileinteraction.jordbruksverketkmm.forms.models.Common
+
+enum class ChecklistType {
+    EDITABLE,
+    LIST,
+}
+
+data class Checklist(
+    val id: String,
+    val title: String,
+    val description: String,
+    val type: ChecklistType,
+    val list: List<ChecklistItem>,
+    val commonData: Common,
+)
--- shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/forms/forms/FormGeneralQuestions.kt
+++ shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/forms/forms/FormGeneralQuestions.kt
@@ -10,96 +10,92 @@ data class FormGeneralQuestions(
     override val type: FormType = FormType.GeneralQuestions,
     override val data: FormData = FormDataGeneralQuestions(),
 ) : Form {
-    override var screens: List<FormScreen> = loadScreens()
-
-    fun loadScreens(): List<FormScreen> {
-        return listOf(
-            FormScreen(
-                components = listOf<FormComponent>(
-                    FormComponentText(
-                        id = "beskrivningTitleScreen1",
-                        type = ComponentType.TITLESMALL,
-                        text = "Beskrivning"
-                    ),
-                    FormComponentText(
-                        id = "beskrivningBodyScreen1",
-                        type = ComponentType.BODY,
-                        text = "Detta test består av allmänna frågor om hur skiftet brukar fungera för din växtodling och om det finns tydliga problem med koppling till markstruktur."
-                    ),
-                    FormComponentImage(
-                        id = "exempelImage",
-                        type = ComponentType.IMAGE,
-                        image = "plant_icon",
-                        caption = "Jordbruksverket",
-                    ),
-                )
-            ),
-            FormScreen(
-                components = listOf<FormComponent>(
-                    FormComponentText(
-                        id = "uppgifterTitleScreen2",
-                        type = ComponentType.TITLESMALL,
-                        text = "Uppgifter om gård och skifte"
-                    ),
-                    FormComponentTextField(
-                        type = ComponentType.TEXTFIELD,
-                        id = ID_FARMNAME,
-                        text = data.commonData.farmInformation.farmName ?: "",
-                        placeholder = "Gårdsnamn",
-                    ),
-                    FormComponentTextField(
-                        type = ComponentType.TEXTFIELD,
-                        id = ID_FARMLAND,
-                        text = data.commonData.farmInformation.farmLand ?: "",
-                        placeholder = "Skifte",
-                    ),
-                    FormComponentTextField(
-                        type = ComponentType.TEXTFIELD,
-                        id = ID_DATE,
-                        text = DateUtils().instantToString(data.commonData.date),
-                        placeholder = "Datum",
-                    ),
-                    FormComponentText(
-                        id = "tipsTitleScreen2",
-                        type = ComponentType.TITLESMALL,
-                        text = "Tips!"
-                    ),
-                    FormComponentText(
-                        id = "tipsBodyScreen2",
-                        type = ComponentType.BODY,
-                        text = "Om du har ett stort skifte..."
-                    ),
+    override var screens: List<FormScreen> = listOf(
+        FormScreen(
+            components = listOf<FormComponent>(
+                FormComponentText(
+                    id = "beskrivningTitleScreen1",
+                    type = ComponentType.TITLESMALL,
+                    text = "Beskrivning"
+                ),
+                FormComponentText(
+                    id = "beskrivningBodyScreen1",
+                    type = ComponentType.BODY,
+                    text = "Detta test består av allmänna frågor om hur skiftet brukar fungera för din växtodling och om det finns tydliga problem med koppling till markstruktur."
+                ),
+                FormComponentImage(
+                    id = "exempelImage",
+                    type = ComponentType.IMAGE,
+                    image = "plant_icon",
+                    caption = "Jordbruksverket",
+                ),
+            )
+        ),
+        FormScreen(
+            components = listOf<FormComponent>(
+                FormComponentText(
+                    id = "uppgifterTitleScreen2",
+                    type = ComponentType.TITLESMALL,
+                    text = "Uppgifter om gård och skifte"
+                ),
+                FormComponentTextField(
+                    type = ComponentType.TEXTFIELD,
+                    id = ID_FARMNAME,
+                    text = data.commonData.farmInformation.farmName ?: "",
+                    placeholder = "Gårdsnamn",
+                ),
+                FormComponentTextField(
+                    type = ComponentType.TEXTFIELD,
+                    id = ID_FARMLAND,
+                    text = data.commonData.farmInformation.farmLand ?: "",
+                    placeholder = "Skifte",
+                ),
+                FormComponentTextField(
+                    type = ComponentType.TEXTFIELD,
+                    id = ID_DATE,
+                    text = DateUtils().instantToString(data.commonData.date),
+                    placeholder = "Datum",
+                ),
+                FormComponentText(
+                    id = "tipsTitleScreen2",
+                    type = ComponentType.TITLESMALL,
+                    text = "Tips!"
+                ),
+                FormComponentText(
+                    id = "tipsBodyScreen2",
+                    type = ComponentType.BODY,
+                    text = "Om du har ett stort skifte..."
                 ),
             ),
-            FormScreen(
-                components = listOf<FormComponent>(
-                    FormComponentText(
-                        id = "grundförutsättningarTitleScreen3",
-                        type = ComponentType.TITLEBIG,
-                        text = "Grundförutsättningar"
-                    ),
-                    FormComponentButtonList(
-                        type = ComponentType.BUTTONLIST,
-                        id = ID_SOILTYPE,
-                        title = "Jordart",
-                        list = listOf("ett", "två"),
-                        value = "ett",
-                        placeholder = "Välj...",
-                    ),
-                    FormComponentText(
-                        id = "tipsTitleScreen3",
-                        type = ComponentType.TITLESMALL,
-                        text = "Tips!"
-                    ),
-                    FormComponentText(
-                        id = "tipsBodyScreen3",
-                        type = ComponentType.BODY,
-                        text = "Om det finns en.."
-                    ),
+        ),
+        FormScreen(
+            components = listOf<FormComponent>(
+                FormComponentText(
+                    id = "grundförutsättningarTitleScreen3",
+                    type = ComponentType.TITLEBIG,
+                    text = "Grundförutsättningar"
+                ),
+                FormComponentButtonList(
+                    type = ComponentType.BUTTONLIST,
+                    id = ID_SOILTYPE,
+                    title = "Jordart",
+                    list = listOf("ett", "två"),
+                    value = "ett",
+                    placeholder = "Välj...",
+                ),
+                FormComponentText(
+                    id = "tipsTitleScreen3",
+                    type = ComponentType.TITLESMALL,
+                    text = "Tips!"
+                ),
+                FormComponentText(
+                    id = "tipsBodyScreen3",
+                    type = ComponentType.BODY,
+                    text = "Om det finns en.."
                 ),
             ),
-        )
-    }
+        ),
+    )
 
     fun setText(id: String, text: String, state: FormViewModel.State): FormViewModel.State {
         println("logg: FORMDEF $text")
@@ -111,7 +107,7 @@ data class FormGeneralQuestions(
             }
         }
 
-        state.form.screens = loadScreens()
+        (screens[state.currentScreen].components.firstOrNull { it.id == id } as FormComponentTextField).text = text
 
         return state
     }
