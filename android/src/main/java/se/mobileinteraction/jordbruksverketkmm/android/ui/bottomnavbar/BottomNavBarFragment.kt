84eaa7c3eb4ed91ed61f0b3f5f016a5a2392033b
David Dahlman <daviddahlman82@gmail.com>
2022-03-15 09:37:20 +0100

    Removed unussed import

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/ui/bottomnavbar/BottomNavBarFragment.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/ui/bottomnavbar/BottomNavBarFragment.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
