8e1a28d8849f7b2ce0f39db8e4c5ddf06b42b069
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-03-16 14:50:13 +0100

    Add place assessment

--- shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/forms/models/FormDataGeneralQuestions.kt
+++ shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/forms/models/FormDataGeneralQuestions.kt
@@ -3,6 +3,7 @@ package se.mobileinteraction.jordbruksverketkmm.forms.models
 data class FormDataGeneralQuestions(
     override val commonData: Common = Common(),
     val soilAssesment: SoilAssesment = SoilAssesment(),
+    val placeAssesment: PlaceAssesment = PlaceAssesment(),
     val questionnaire: Questionnaire = Questionnaire(),
     var comment: String = "",
 ) : FormData
