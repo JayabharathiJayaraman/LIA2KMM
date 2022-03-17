eb9de9ae1bcf8f96c8fa24fbdc9f03935385e281
David Dahlman <daviddahlman82@gmail.com>
2022-03-17 12:46:02 +0100

    Update MainApplication.kt so checklistViewModel remember state when in the app.

--- android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/MainApplication.kt
+++ android/src/main/java/se/mobileinteraction/jordbruksverketkmm/android/MainApplication.kt
@@ -9,7 +9,8 @@ import se.mobileinteraction.jordbruksverketkmm.forms.forms.FormGeneralQuestions
 import se.mobileinteraction.jordbruksverketkmm.forms.forms.FormType
 
 class MainApplication : Application() {
-    var formViewModel: FormViewModel = FormViewModel(FormGeneralQuestions(FormType.GeneralQuestions))
+    var formViewModel: FormViewModel =
+        FormViewModel(FormGeneralQuestions(FormType.GeneralQuestions))
 
     private val grundforbattringarState = listOf<ChecklistItem>(
         ChecklistItem("Huvudavvattning", true, true),
@@ -35,13 +36,31 @@ class MainApplication : Application() {
         ChecklistItem("MinimeraAndelenBarMark", true, true)
     )
 
-    var checklistViewModel: ChecklistViewModel = ChecklistViewModel(Checklist(Checklist.Category.GRUNDFORBATTRINGAR, grundforbattringarState), 0)
+    private var grundforbattringarChecklist: ChecklistViewModel = ChecklistViewModel(
+        Checklist(
+            Checklist.Category.GRUNDFORBATTRINGAR,
+            grundforbattringarState
+        ), 0
+    )
+    private var odlingsatgarderChecklist: ChecklistViewModel = ChecklistViewModel(
+        Checklist(
+            Checklist.Category.ODLINGSATGARDER,
+            odlingsatgarderState
+        ), 0
+    )
+    private var undvikellerminimerChecklist: ChecklistViewModel = ChecklistViewModel(
+        Checklist(
+            Checklist.Category.UNDVIKELLERMINIMERA,
+            undvikEllerMinimeraState
+        ), 0
+    )
+    var checklistViewModel: ChecklistViewModel = grundforbattringarChecklist
 
-    fun setChecklistViewModel(category: Checklist.Category){
-        when(category){
-            Checklist.Category.GRUNDFORBATTRINGAR -> checklistViewModel = ChecklistViewModel(Checklist(Checklist.Category.GRUNDFORBATTRINGAR, grundforbattringarState), 0)
-            Checklist.Category.ODLINGSATGARDER-> checklistViewModel = ChecklistViewModel(Checklist(Checklist.Category.ODLINGSATGARDER, odlingsatgarderState), 0)
-            Checklist.Category.UNDVIKELLERMINIMERA -> checklistViewModel = ChecklistViewModel(Checklist(Checklist.Category.UNDVIKELLERMINIMERA, undvikEllerMinimeraState), 0)
+    fun setChecklistViewModel(category: Checklist.Category) {
+        checklistViewModel = when (category) {
+            Checklist.Category.GRUNDFORBATTRINGAR -> grundforbattringarChecklist
+            Checklist.Category.ODLINGSATGARDER -> odlingsatgarderChecklist
+            Checklist.Category.UNDVIKELLERMINIMERA -> undvikellerminimerChecklist
         }
     }
 
