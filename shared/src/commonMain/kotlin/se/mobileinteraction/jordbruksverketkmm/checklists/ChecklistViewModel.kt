d9da9a455ae23382983ef9af0222e3c2a58e9420
David Dahlman <daviddahlman82@gmail.com>
2022-03-17 10:33:40 +0100

    Cleaned up triggerStateActive function.

--- shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/checklists/ChecklistViewModel.kt
+++ shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/checklists/ChecklistViewModel.kt
@@ -1,6 +1,5 @@
 package se.mobileinteraction.jordbruksverketkmm.checklists
 
-import android.util.Log
 import se.mobileinteraction.jordbruksverketkmm.Checklist
 import se.mobileinteraction.jordbruksverketkmm.checklists.models.ChecklistItem
 import se.mobileinteraction.jordbruksverketkmm.utilities.ViewModelState
@@ -8,33 +7,39 @@ import se.mobileinteraction.jordbruksverketkmm.utilities.ViewModelStateImpl
 
 class ChecklistViewModel constructor(
     val checklist: Checklist,
-    val count: Int
-): ViewModelState<ChecklistViewModel.State> by ViewModelStateImpl(State(checklist, count)) {
-    data class State(val checklist: Checklist,val count: Int){}
+    private val count: Int
+) : ViewModelState<ChecklistViewModel.State> by ViewModelStateImpl(State(checklist, count)) {
+    data class State(val checklist: Checklist, val count: Int)
 
     fun triggerStateActive(itemName: String) {
-        val tmp = state.value.checklist
-        Log.d("wtf", tmp.itemList.toString())
-        val tmp2 = tmp.itemList.filter { it.id == itemName }[0].copy(active = !tmp.itemList.filter { it.id == itemName }[0].active)
-        val newStateList = mutableListOf<ChecklistItem>()
+        val updatedChecklistItem = state.value.checklist.itemList
+            .first { it.id == itemName }
+            .copy(active = !state.value.checklist.itemList
+                .first { it.id == itemName }.active
+            )
+        val newChecklistItemList: MutableList<ChecklistItem> = mutableListOf<ChecklistItem>()
 
-        for (elem in tmp.itemList) {
-            if (elem.id == checklist.itemList.filter { it.id == itemName }[0].id) {
-                newStateList.add(tmp2)
-                Log.d("this change",tmp2.toString())
+        for (elem in state.value.checklist.itemList) {
+            if (elem.id == checklist.itemList.first { it.id == itemName }.id
+            ) {
+                newChecklistItemList.add(updatedChecklistItem)
             } else {
-                newStateList.add(elem)
-                Log.d("this haven't change",elem.toString())
+                newChecklistItemList.add(elem)
             }
         }
-        updateStateAndSave { copy(checklist = Checklist(checklist.category, newStateList), count =  state.value.count + 1) }
+        updateStateAndSave {
+            copy(
+                checklist = Checklist(checklist.category, newChecklistItemList),
+                count = state.value.count + 1
+            )
+        }
     }
 
-    private fun updateStateAndSave(state: ChecklistViewModel.State.() -> ChecklistViewModel.State) {
+    private fun updateStateAndSave(state: State.() -> State) {
         updateState(state).also(::save)
     }
 
-    private fun save(state: ChecklistViewModel.State) {
+    private fun save(state: State) {
         println("Saving state: $state")
     }
 }
