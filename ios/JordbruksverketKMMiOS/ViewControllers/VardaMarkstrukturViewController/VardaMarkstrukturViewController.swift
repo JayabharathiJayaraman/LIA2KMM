4dfd99b64115fc295de5304781333be6e0bdd67e
David Dahlman <daviddahlman82@gmail.com>
2022-03-20 13:01:40 +0100

    Added Text components

--- ios/JordbruksverketKMMiOS/ViewControllers/VardaMarkstrukturViewController/VardaMarkstrukturViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/VardaMarkstrukturViewController/VardaMarkstrukturViewController.swift
@@ -9,12 +9,30 @@ import UIKit
 
 class VardaMarkstrukturViewController: UIViewController {
 
+    @IBOutlet weak var Label_1: UILabel!
+    
+    @IBOutlet weak var MainText: UILabel!
+    
+    @IBOutlet weak var SecondText: UILabel!
+    
+    @IBOutlet weak var Label_2: UILabel!
+    
+  
+    @IBOutlet weak var PDF_Btn_label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpLabels()
     }
 
+    func setUpLabels(){
+        Label_1.text = "varda_markstruktur_beskrivning".localized
+        MainText.text = "varda_markstruktur_main_text".localized
+        SecondText.text = "varda_markstruktur_second_text".localized
+        Label_2.text = "varda_markstruktur_atgarder".localized
+        PDF_Btn_label.text = "varda_markstruktur_export_button".localized
+    }
 
     /*
     // MARK: - Navigation
