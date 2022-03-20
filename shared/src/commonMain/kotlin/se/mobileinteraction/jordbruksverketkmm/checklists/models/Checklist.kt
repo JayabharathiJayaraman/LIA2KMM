bd308e64f2fee2cea121458f16f929e8e8004876
David Dahlman <daviddahlman82@gmail.com>
2022-03-20 13:33:41 +0100

    Moved logics for creating checklistItem from MainApplication.kt to Checklist.kt

--- shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/checklists/models/Checklist.kt
+++ shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/checklists/models/Checklist.kt
@@ -4,9 +4,9 @@ import se.mobileinteraction.jordbruksverketkmm.checklists.models.ChecklistItem
 
 data class Checklist(val category: Category, val itemList: List<ChecklistItem>) {
 
-    val title : String = "CheckList_" + category.categoryName + "_title"
-    val text : String = "CheckList_" + category.categoryName + "_text"
-    val id : Category = category
+    val title: String = "CheckList_" + category.categoryName + "_title"
+    val text: String = "CheckList_" + category.categoryName + "_text"
+    val id: Category = category
 
     enum class Category(val categoryName: String) {
         GRUNDFORBATTRINGAR("Grundförbättringar"),
@@ -14,4 +14,33 @@ data class Checklist(val category: Category, val itemList: List<ChecklistItem>) 
         UNDVIKELLERMINIMERA("UndvikEllerMinimera")
     }
 
+    enum class ItemStates(val state: List<ChecklistItem>) {
+        GRUNDFORBATTRINGAR(
+            listOf<ChecklistItem>(
+                ChecklistItem("Huvudavvattning", true, true),
+                ChecklistItem("OrganiskaMaterial", true, true),
+                ChecklistItem("Detaljdränering", true, true),
+                ChecklistItem("Strukturkalkning", true, true),
+                ChecklistItem("Alvluckring", true, true)
+            )
+        ),
+        ODLINGSATGARDER(
+            listOf<ChecklistItem>(
+                ChecklistItem("GynnaDaggmaskarna", true, true),
+                ChecklistItem("GrödorMedBraRotsystem", true, true),
+                ChecklistItem("LämnaOrganisktMaterial", true, true),
+                ChecklistItem("PlaneraKörningen", true, true),
+                ChecklistItem("MinskaBelastningenPåMarken", true, true),
+                ChecklistItem("BeväxtMarkÅretOm", true, true)
+            )
+        ),
+        UNDVIKELLERMINIMERA(
+            listOf<ChecklistItem>(
+                ChecklistItem("UndvikTungaMaskiner", true, true),
+                ChecklistItem("UndvikKörningVidVåtaMarkförhållanden", true, true),
+                ChecklistItem("MinimeraAntaletÖverfarter", true, true),
+                ChecklistItem("MinimeraAndelenBarMark", true, true)
+            )
+        )
+    }
 }
