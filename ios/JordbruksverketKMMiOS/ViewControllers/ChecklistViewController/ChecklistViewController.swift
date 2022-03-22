cca3c27db0756a6e674c5f2c43d190cc820f1927
David Dahlman <daviddahlman82@gmail.com>
2022-03-22 13:18:06 +0100

    Testing data in ChecklistViewController.swift

--- ios/JordbruksverketKMMiOS/ViewControllers/ChecklistViewController/ChecklistViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/ChecklistViewController/ChecklistViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ChecklistViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    var testVar = IOSChecklistViewModel.shared
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        testLabel.text = String(testVar.currentState.checklist.category.categoryName)
+        
     }
 
 
