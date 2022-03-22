1824f0a098694a485b1b822e2e98a61b864dcee8
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-03-22 15:16:54 +0100

    Add function to set questionnaire answer

--- shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/forms/forms/Form.kt
+++ shared/src/commonMain/kotlin/se/mobileinteraction/jordbruksverketkmm/forms/forms/Form.kt
@@ -3,6 +3,7 @@ package se.mobileinteraction.jordbruksverketkmm.forms.forms
 import se.mobileinteraction.jordbruksverketkmm.forms.FormViewModel
 import se.mobileinteraction.jordbruksverketkmm.forms.components.FormScreen
 import se.mobileinteraction.jordbruksverketkmm.forms.models.FormData
+import se.mobileinteraction.jordbruksverketkmm.forms.models.QuestionnaireAnswer
 
 interface Form {
     val screens: List<FormScreen>
@@ -13,7 +14,7 @@ interface Form {
     fun setChecklistActive(id: String, active: Int, state: FormViewModel.State): FormViewModel.State
     fun setQuestionnaireAnswer(
         id: String,
-        answer: Int,
+        answer: QuestionnaireAnswer,
         state: FormViewModel.State
     ): FormViewModel.State
 
