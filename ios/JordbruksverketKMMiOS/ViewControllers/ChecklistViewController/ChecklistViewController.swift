9c4b292d2a100743d0c0ccc7ac891831f208c535
David Dahlman <daviddahlman82@gmail.com>
2022-03-23 14:07:24 +0100

    Addet text for Ej aktuella åtgärder and set to hiden if Undvik eller minimera

--- ios/JordbruksverketKMMiOS/ViewControllers/ChecklistViewController/ChecklistViewController.swift
+++ ios/JordbruksverketKMMiOS/ViewControllers/ChecklistViewController/ChecklistViewController.swift
@@ -19,14 +19,29 @@ class ChecklistViewController: UIViewController {
    
     @IBOutlet weak var AtgardslistaTextLabel: UILabel!
     
+    @IBOutlet weak var EjAktullaLabel: UILabel!
+  
+    @IBOutlet weak var EjAktuellaTextLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       setLabels()
+    }
+
+
+    func setLabels(){
         TitleLabel.text = String(IOSChecklistViewModel.shared.currentState.checklist.title).localized
         MainTextLabel.text = String(IOSChecklistViewModel.shared.currentState.checklist.text).localized
         AnpassaTextLabel.text = "check_list_text1".localized
         AtgardlistaLabel.text = "check_list_label_atgardslista".localized
         AtgardslistaTextLabel.text = "check_list_text_3".localized
+        EjAktullaLabel.text = "check_list_label_ej_aktuella_atgarder".localized
+        EjAktuellaTextLabel.text = "check_list_text_4".localized
+        
+        if(IOSChecklistViewModel.shared.currentState.checklist.category == IOSChecklistViewModel.undvikViewModel.checklist.category){
+            EjAktullaLabel.isHidden = true
+            EjAktuellaTextLabel.isHidden = true
+        }
     }
-
-
 }
